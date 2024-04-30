17a5d2e07f56ea4fa3fa6a08412f3fcbefe631eb
Bensonodhiambo <benodhiambo.bo@gmail.com>
2024-04-29 20:20:48 -0700

    AdvancedTask

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-#using puppet to make changes to out configuration file
-
-file {'etc/ssh/ssh_config':
-	ensure => present,
-
-content =>"
-	#SSH client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentification no
-	",
+# Using puppet to make changes to our configuration file
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => '
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ',
 }
+
