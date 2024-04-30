87ec59227d61f7a5e6f96786a247859b9e817d9e
Bensonodhiambo <benodhiambo.bo@gmail.com>
2024-04-29 20:29:01 -0700

    AdvancedTask

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Using puppet to make changes to our configuration file
-file { '/etc/ssh/ssh_config':
+file { '/ect/ssh/ssh_config':
   ensure  => present,
   content => '
     # SSH client configuration
