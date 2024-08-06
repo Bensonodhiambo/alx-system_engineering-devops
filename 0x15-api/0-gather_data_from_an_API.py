622fc9202e34887899fc2442ab34122244987ff9
Bensonodhiambo <benodhiambo.bo@gmail.com>
2024-08-05 20:53:08 -0700

    Task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,27 +4,37 @@ import requests
 from sys import argv
 """modules to work with"""
 
-if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
 
-    response = requests.get(url)
-    employeeName = response.json().get('name')
+def get_employee_todos_progress(employee_id):
+    """ A function to return the info about employees todos progress"""
+    try:
+        url = "https://jsonplaceholder.typicode.com/"
+        user_datas = requests.get(url + f"users/{employee_id}")
+        user_data = user_datas.json()
+        employee_name = user_data["name"]
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-    done = 0
-    done_tasks = []
+        """ Fetch employee todos list"""
+        todos_list = requests.get(url + f"todos?userid={employee_id}")
+        json_todos_list = todos_list.json()
 
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
+        total_task = len(json_todos_list)
+        task_done = [task for task in json_todos_list if task['completed']]
+        no_task_done = len(task_done)
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
+        """Display the results"""
+        print(f"Employee {employee_name} is done with tasks"
+              f"{no_task_done}/{total_task}:")
 
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
+        """" Title of completed tasks"""
+        for task in task_done:
+            print(f"\t {task['title']}")
+
+    except Exception as e:
+        print(f"an error has occured: {e}")
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: script <employee_id>")
+    else:
+        get_employee_todos_progress(argv[1])
